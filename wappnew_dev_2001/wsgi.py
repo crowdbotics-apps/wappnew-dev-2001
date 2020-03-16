3167477846136a4b786a51c2997131c0c8071318
Crowdbotics <team@crowdbotics.com>
2020-03-16 11:43:33 +0000

    Update models

--- wappnew_dev_2001/wsgi.py
+++ wappnew_dev_2001/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wappnew_dev_2001.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wappnew_dev_2001.settings")
 
 application = get_wsgi_application()
